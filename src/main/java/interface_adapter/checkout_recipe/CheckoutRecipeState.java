789de413a7aed63f3146e3fe8dd8e9e43491ec3b
flypt7 <tomsarrazin01@gmail.com>
2025-11-29 17:18:46 -0500

    updated to separate recipe info and recipe ingredients

--- src/main/java/interface_adapter/checkout_recipe/CheckoutRecipeState.java
+++ src/main/java/interface_adapter/checkout_recipe/CheckoutRecipeState.java
@@ -1,19 +1,30 @@
 package interface_adapter.checkout_recipe;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CheckoutRecipeState {
 
-    private Map<String, Object> recipeInfo = new HashMap<>();
+    private Map<String, String> recipeInfo = new HashMap<>();
+    private List<ArrayList<String>> recipeIngredients = new ArrayList<>();
     private String errorMessage = null;
 
-    public Map<String, Object> getRecipeInfo() {
+    public Map<String, String> getRecipeInfo() {
         return recipeInfo;
     }
 
-    public void setRecipeInfo(Map<String, Object> recipeInfo) {
+    public void setRecipeInfo(Map<String, String> recipeInfo) {
         this.recipeInfo = recipeInfo;
+    }
+
+    public List<ArrayList<String>> getRecipeIngredients() {
+        return recipeIngredients;
+    }
+
+    public void setRecipeIngredients(List<ArrayList<String>> recipeIngredients) {
+        this.recipeIngredients = recipeIngredients;
     }
 
     public String getErrorMessage() {
