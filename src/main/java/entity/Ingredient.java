2d5e42d04325e839b39a65ca9db9366c9ec95d7f
aaliyahpirani <aaliyah.happy@gmail.com>
2025-12-01 20:37:27 -0500

    Fixed ingredients so that all of the tests pass.

--- src/main/java/entity/Ingredient.java
+++ src/main/java/entity/Ingredient.java
@@ -1,12 +1,16 @@
 package entity;
 
+import java.util.Objects;
+
 public class Ingredient {
     private final String name;
     private final String quantity;
     private final String unit;
 
     public Ingredient(String name, String quantity, String unit) {
-        if (name == null || name.isEmpty()) throw new IllegalArgumentException("Name required");
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name required");
+        }
         this.name = name;
         this.quantity = quantity;
         this.unit = unit;
@@ -22,5 +26,29 @@ public class Ingredient {
 
     public String getUnit() {
         return unit;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" +
+                "name='" + name + '\'' +
+                ", quantity='" + quantity + '\'' +
+                ", unit='" + unit + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Ingredient)) return false;
+        Ingredient that = (Ingredient) o;
+        return Objects.equals(name, that.name) &&
+                Objects.equals(quantity, that.quantity) &&
+                Objects.equals(unit, that.unit);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, quantity, unit);
     }
 }
