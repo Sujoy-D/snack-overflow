7b1a3b8e4aff2916565411bc017956556de88d94
flypt7 <tomsarrazin01@gmail.com>
2025-12-02 03:25:03 -0500

    created checked exception to handle case when interactor receives a null recipe from InputData.

---
+++ src/main/java/use_case/checkout_recipe/NullRecipeException.java
@@ -0,0 +1,16 @@
+package use_case.checkout_recipe;
+
+import java.io.IOException;
+
+public class NullRecipeException extends IOException {
+    private final String message;
+
+    public NullRecipeException() {
+        message = "Null Recipe - no data can be extracted.";
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
